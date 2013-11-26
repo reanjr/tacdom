e2e5f68a8b4682c98155b122ce87de82b6137963
Rich Remer <reanjr@gmail.com>
2013-11-26 12:12:22 -0800

    configured sessions

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 var express = require("express"),
+    redis = require("redis").createClient(),
+    RedisStore = require("connect-redis")(express),
     app = express();
 
+// serve static assets from public folder
 app.use(express.static("public"));
 
+// enable cookies
+app.use(express.cookieParser());
+
+// session middle-ware using redis for storage
+app.use(express.session({
+    secret: "the secret ingredient is love, dammit",
+    store: new RedisStore({client: redis})
+}));
+
+// redirect homepage to appropriate static asset
 app.get("/", function(req, rsp) {
     rsp.redirect("/tactical-domination.html");
 });
 
+// begin listening
 app.listen(1007);
