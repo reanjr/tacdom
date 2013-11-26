81865c76bffb9d54b577119a7c2c85f9b1803fa6
Rich Remer <reanjr@gmail.com>
2013-11-26 11:40:25 -0800

    added empty game homepage

--- server.js
+++ server.js
@@ -2,4 +2,9 @@ var express = require("express"),
     app = express();
 
 app.use(express.static("public"));
+
+app.get("/", function(req, rsp) {
+    rsp.redirect("/tactical-domination.html");
+});
+
 app.listen(1007);
