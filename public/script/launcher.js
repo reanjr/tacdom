c937a75b3da1cd307f95221df8c549b8cab3265f
Rich Remer <reanjr@gmail.com>
2013-12-06 15:01:05 -0800

    setup Space in game view

--- public/script/launcher.js
+++ public/script/launcher.js
@@ -1,9 +1,9 @@
-require([], function() {
+require(["view/space"], function(Space) {
     // find view target
     var target = document.getElementById("tacdom")
             || document.getElementsByClassName("tacdom")[0]
             || document.getElementsByTagName("body")[0];
 
-    if (!target) console.error("could not identify view target");
-    else console.log(target);
+    // create a space view for the game
+    var space = Space.create(target);
 });
