1f70b19aa00f761e4461135a8a141eb174d82d1f
Rich Remer <reanjr@gmail.com>
2013-12-06 14:58:28 -0800

    Space prototype; View with many child views, shows one at a time

---
+++ public/script/view/space.js
@@ -0,0 +1,82 @@
+define(["wire/view"], function(View) {
+
+    var Space = Object.create(View);
+
+    Space.views = [];
+    Space.active = null;
+
+    Space.create = function(elem) {
+        var obj = (this == Space) ? Object.create(Space) : this;
+        View.create.call(obj, elem);
+
+        // if Space already has content, add it as a view
+        var content = obj.elem.innerHTML.trim();
+        if (content) {
+            obj.elem.innerHTML = "";
+            obj.show(content);
+        }
+    }
+
+    Space.activate = function(view) {
+        if (view instanceof String) {
+            for (var i in this.views) {
+                if (this.views[i].dataset.name == view) {
+                    view = this.views[i];
+                }
+            }
+        }
+
+        for (var i in this.views) {
+            var view = this.views[i];
+            if (view.dataset.name == name) {
+                var newView = view,
+                    activeView = this.active;
+
+                this.active = newView;
+                newView.classList.add("active");
+                if (activeView) activeView.classList.remove("active");
+
+                this.trigger("activate", newView, activeView);
+                return;
+            }
+        }
+    };
+
+    Space.add = function(name, content) {
+        var newView = this.createView(content, name);
+
+        this.views.unshift(newView);
+        this.trigger("add", newView);
+        this.elem.appendChild(newView);
+
+        if (!this.active)
+            this.activate(name);
+    };
+
+    Space.createView = function(content, name) {
+        var container = document.createElement("div");
+
+        container.classList.add("-space-view");
+        container.innerHTML = content;
+
+        if (name) container.dataset.name = name;
+
+        return container;
+    };
+
+    Space.show = function(content) {
+        var newView = this.createView(content),
+            activeView = this.active;
+
+        this.views.push(newView);
+        this.active = newView;
+        this.trigger("activate", newView, activeView);
+
+        newView.classList.add("active");
+        if (activeView) activeView.classList.remove("active");
+        this.elem.appendChild(newView);
+    }
+
+    return Space;
+
+});
