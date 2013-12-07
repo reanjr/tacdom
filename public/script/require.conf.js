460aa6c6fdf1c87046445d7eb7998f5b8f2cebdf
Rich Remer <reanjr@gmail.com>
2013-12-06 19:35:28 -0800

    configure stache dependencies in require.js

--- public/script/require.conf.js
+++ public/script/require.conf.js
@@ -1,7 +1,10 @@
 var require = {
     baseUrl: "/script",
     paths: {
+        "text": "//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.10/text",
+        "Mustache": "//cdnjs.cloudflare.com/ajax/libs/mustache.js/0.7.2/mustache.min",
         "stache": "//cdnjs.cloudflare.com/ajax/libs/requirejs-mustache/0.0.2/stache",
         "domReady": "//cdnjs.cloudflare.com/ajax/libs/require-domReady/2.0.1/domReady"
-    }
+    },
+    stache: {extension: ".stache"}
 }
