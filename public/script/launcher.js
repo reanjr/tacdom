fab003db5d5e53b1b4c5e5fa4a6c6592915ee2cd
Rich Remer <reanjr@gmail.com>
2013-12-06 19:43:45 -0800

    show commander name input on load

--- public/script/launcher.js
+++ public/script/launcher.js
@@ -1,4 +1,4 @@
-require(["view/space"], function(Space) {
+require(["require", "view/space"], function(require, Space) {
     // find view target
     var target = document.getElementById("tacdom")
             || document.getElementsByClassName("tacdom")[0]
@@ -6,4 +6,9 @@ require(["view/space"], function(Space) {
 
     // create a space view for the game
     var space = Space.create(target);
+
+    // get commander name from user
+    require(["stache!/template/text-box"], function(textBox) {
+        space.show(textBox({caption: "What is your commander name?"}));
+    });
 });
