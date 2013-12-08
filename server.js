85539f7006c2dee534c73bdbba7c4fc018042dd4
Rich Remer <reanjr@gmail.com>
2013-12-07 18:02:20 -0800

    service for creating a new commander

--- server.js
+++ server.js
@@ -23,9 +23,60 @@ app.use(express.session({
     store: new RedisStore({client: redis})
 }));
 
+// enable parsed post data
+app.use(express.bodyParser());
+
 // redirect homepage to appropriate static asset
 app.get("/", function(req, rsp) {
     rsp.redirect("/tacdom.html");
+});
+
+// unit data
+app.get("/unit/:id", function(req, rsp) {
+    var host = req.headers.host + ":" + app.get("port"),
+        baseUri = "//" + host;
+
+    redis.hgetall(baseUri + req.url, function(err, reply) {
+        if (err) {
+            rsp.status(404);
+            rsp.end();
+        } else {
+            rsp.setHeader("Content-Type", "application/json");
+            rsp.end(JSON.stringify(reply));
+        }
+    });
+});
+
+// create a new commander for the current session
+app.post("/commander", function(req, rsp) {
+    var host = req.headers.host + ":" + app.get("port"),
+        baseUri = "//" + host,
+        idUri = "/id",
+        name = req.body.name,
+        commander = {
+            id: null, uri: "/unit/", name: name,
+            job: "commander",
+            hea: 10, atk: 2, def: 2, spd: 2,
+            hp: 10, act: 2,
+            weap_dmg: 1, weap_rng: 1,
+            special: "command"
+        };
+
+    if (!name) {
+        rsp.status(500);
+        rsp.end();
+        return;
+    }
+
+    redis.incr(baseUri + idUri, function(err, reply) {
+        // TODO: handle err
+        commander.id = reply;
+        commander.uri += reply;
+        redis.hmset(baseUri + commander.uri, commander, function(err, reply) {
+            // TODO: handle err
+            rsp.redirect(303, commander.uri);
+        });
+    });
 });
 
 // begin listening
