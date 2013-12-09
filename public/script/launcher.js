398ff8d412bd3c98bcc5317f31c24724ed6959ff
Rich Remer <reanjr@gmail.com>
2013-12-08 21:48:29 -0800

    ui module to simplify loading views

--- public/script/launcher.js
+++ public/script/launcher.js
@@ -1,11 +1,11 @@
-require(["require", "view/space", "data/namer"], function(require, Space, Namer) {
+require(["require", "view/ui", "data/namer"], function(require, ui, Namer) {
     // find view target
     var target = document.getElementById("tacdom")
             || document.getElementsByClassName("tacdom")[0]
             || document.getElementsByTagName("body")[0];
 
     // create a space view for the game
-    var space = Space.create(target);
+    var space = ui.Space.create(target);
 
     // get commander name from user
     require(["stache!/template/text-box"], function(textBox) {
---
+++ public/script/view/ui.js
@@ -0,0 +1,8 @@
+define(["view/space"],
+function(Space) {
+
+    return {
+        Space: Space
+    };
+
+});
